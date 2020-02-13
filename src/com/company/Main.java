dba2b5f98ffc54c3e91f969e7aab29d8d170b9ef
mwillib <mike.williamsbackshall@aexp.com>
2020-02-13 14:18:21 +0000

    2D Array and Basic Check Functions

---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[][] board = {
+                {0, 0, 0, 0, 0, 2, 1, 0, 0},
+                {0, 0, 4, 0, 0, 8, 7, 0, 0},
+                {0, 2, 0, 3, 0, 0, 9, 0, 0},
+                {6, 0, 2, 0, 0, 3, 0, 4, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 5, 0, 6, 0, 0, 3, 0, 1},
+                {0, 0, 3, 0, 0, 5, 0, 8, 0},
+                {0, 0, 8, 2, 0, 0, 5, 0, 0},
+                {0, 0, 9, 7, 0, 0, 0, 0, 0}
+        };
+
+    }
+
+    private boolean inRow(int row, int num) {
+
+    }
+
+    private boolean inCol(int col, int num) {
+
+    }
+
+    private boolean inBox(int row, int col, int num) {
+
+    }
+
+}
