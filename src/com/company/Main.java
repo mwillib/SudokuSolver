b29d29692baf049dcf6191228b08a71d6fa73936
mwillib <mike.williamsbackshall@aexp.com>
2020-02-13 14:51:55 +0000

    Three Checks

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -18,16 +18,37 @@ public class Main {
 
     }
 
-    private boolean inRow(int row, int num) {
+    private boolean inRow(int[][] board, int row, int num) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == num) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean inCol(int[][] board, int col, int num) {
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] == num) {
+                return true;
+            }
+        }
+        return false;
 
     }
 
-    private boolean inCol(int col, int num) {
+    private boolean inBox(int[][] board, int row, int col, int num) {
+        int r = row - row % 3;
+        int c = col - col % 3;
 
-    }
-
-    private boolean inBox(int row, int col, int num) {
-
+        for (int i = r; i < r + 3; i++) {
+            for (int j = c; j < c + 3; j++) {
+                if (board[i][j] == num) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
 }
